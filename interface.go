edc24adf683b40e2c3a5123cf6ca1f5254adea74
Eduard Urbach <e.urbach@gmail.com>
2019-10-14 17:15:17 +0900

    Removed unused code

--- interface.go
+++ interface.go
@@ -9,15 +9,6 @@ type iface struct {
 	data unsafe.Pointer
 }
 
-// pack creates a new interface from the type and data pointers.
-func pack(typ unsafe.Pointer, data unsafe.Pointer) interface{} {
-	var obj interface{}
-	packed := (*iface)(unsafe.Pointer(&obj))
-	packed.typ = typ
-	packed.data = data
-	return packed
-}
-
 // unpack lets you access the internal interface data for the object.
 func unpack(obj interface{}) *iface {
 	return (*iface)(unsafe.Pointer(&obj))
